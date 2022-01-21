93cda6f69354bdb14c20f5735e18ce3a2e7f73f9
elisha <elishabello2014@gmail.com>
2022-01-21 23:25:05 +0100

    security patch

--- models/index.js
+++ models/index.js
@@ -14,7 +14,7 @@ import { default as dbConfig } from "../config/db.config.js"
 //     },
 // });
 const sequelize = new Sequelize(
-    "postgres://ayztrtrlucbmoc:06b8fffd8a04a73ad18f64edf5bd4376f96cf47eaad03a99ceb9552c75fbae51@ec2-54-198-213-75.compute-1.amazonaws.com:5432/d7jn1jenkjed88\n",
+    process.env.DB_URL, // your database url
     {
         dialect: 'postgres',
         protocol: 'postgres',
